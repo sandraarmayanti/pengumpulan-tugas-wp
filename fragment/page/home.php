761b322c57a0dc55cc5307eee741ba07a9081919
sandraarmayanti <sandraarmayanti@gmail.com>
2022-06-24 13:51:36 +0700

    first commit

---
+++ fragment/page/home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penilaian Barang</title>
+   
+</head>
+
+<body>
+    <div class="main">
+        <header>
+            <center>
+                <h1><u> Sistem Informasi Penilaian Barang </u></h1>
+            </center>
+        </header>
+         <div class="pertama">
+            <center>
+            <div>
+                <img src="http://2.bp.blogspot.com/-H6FYJBrtIwA/VAmRnaUimLI/AAAAAAAAADQ/W3ZDugzWyA0/s1600/531212430c64e_thumb900.jpg" height="360" width="350"/>
+            </div>
+
+            <hgroup>
+                <h2>Penilaian Data Barang</h2>
+            </hgroup>
+             <p>penilaian merupakan proses kegiatan untuk mengetahui apakah suatu program yang sudah ditetapkan sebelumnya berhasil dengan baik atau tidak baik. Agar mengetahui informasi mengenai penilaian tersebut, digunakan pengukuran, baik itu menggunakan instrumen tes maupun nontes.</p>
+            </article>
+        </div>
+            </center>
+            
+            
+           
+    </div>
+</body>
+
+</html>
